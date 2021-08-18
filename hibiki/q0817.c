62ce2e40ed2058e9e52061fd88adabf1e52d8b87
hibiki1228 <devkimadore1228@gmail.com>
2021-08-18 23:27:58 +0900

    answer q0816 & q0817 & q0818

---
+++ hibiki/q0817.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  int i;
+  scanf("%d", &i);
+
+  return i + 1;
+}
---
+++ hibiki/q0818.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int num;
+  scanf("%d",&num);
+
+  if (num % 2 == 0) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
