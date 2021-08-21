3f3738d9a7547f8d54e6ca860fcc0862a8f4de73
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-21 15:16:20 +0900

    Fix the direction of the inequality sign

--- take_cantik/q_0821.c
+++ take_cantik/q_0821.c
@@ -10,11 +10,11 @@ int main(void) {
 
   bmi = weight / (height * height);
 
-  if (bmi <= 18.5 && bmi > 25) {
+  if (bmi >= 18.5 && bmi < 25) {
     printf("普通\n");
   } else if (bmi < 18.5) {
     printf("痩せ型\n");
-  } else if (bmi <= 25) {
+  } else if (bmi >= 25) {
     printf("肥満\n");
   }
 
