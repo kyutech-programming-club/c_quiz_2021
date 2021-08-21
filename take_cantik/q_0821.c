23dd6a2480e9ccce94e7ede655d64431d1ae3f6f
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-21 15:10:49 +0900

    Solve 0821

--- take_cantik/q_0821.c
+++ take_cantik/q_0821.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
+  double height, weight, bmi;
+  printf("身長[m]と体重[kg]を入力してください\n");
+  printf("身長[m]：");
+  scanf("%lf", &height);
+  printf("体重[kg]：");
+  scanf("%lf", &weight);
+
+  bmi = weight / (height * height);
+
+  if (bmi <= 18.5 && bmi > 25) {
+    printf("普通\n");
+  } else if (bmi < 18.5) {
+    printf("痩せ型\n");
+  } else if (bmi <= 25) {
+    printf("肥満\n");
+  }
+
   return 0;
 }
 
