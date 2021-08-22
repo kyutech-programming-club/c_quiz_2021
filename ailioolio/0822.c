722e7e15f0e66e73884b38e220e0df0117fa55c5
ailioolio <aiai4849@gmail.com>
2021-08-22 11:37:52 +0900

    whileよりforのほうがきれいですね

--- ailioolio/0822.c
+++ ailioolio/0822.c
@@ -3,21 +3,17 @@
 int main(void)
 {
   int num1, num2;
-  printf("総和を求める（num%d < num%d)\n", 1, 2);
-  printf("num%d: ", 1);
+  printf("総和を求める（num1 < num2)\n");
+  printf("num1: ");
   scanf("%d", &num1);
-  printf("num%d: ", 2);
+  printf("num2: ");
   scanf("%d", &num2);
 
-  if (num1 < num2)
+  int sum = 0;
+  for (num1; num1 <= num2; num1++)
   {
-    int sum = (num1 + num2) * (num2 - num1 + 1) / 2;
-    printf("総和は%dです。\n", sum);
+    sum += num1;
   }
-  else
-  {
-    printf("error\n");
-  }
-
+  printf("%d\n", sum);
   return 0;
 }
