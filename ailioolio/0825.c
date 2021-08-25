553f8144a966d0a182916b9f892d87b8c4e4ad34
ailioolio <aiai4849@gmail.com>
2021-08-25 21:05:52 +0900

    change condition 2

--- ailioolio/0825.c
+++ ailioolio/0825.c
@@ -5,27 +5,21 @@ int main(void)
 
   for (int i = 1; i <= 100; i++)
   {
-    if (i % 3 != 0)
+    if (i % 15 == 0)
     {
-      if (i % 5 != 0)
-      {
-        printf("%d\n", i);
-      }
-      else
-      {
-        printf("Buzz\n");
-      }
+      printf("FizzBuzz\n");
+    }
+    else if (i % 3 == 0)
+    {
+      printf("Fizz\n");
+    }
+    else if (i % 5 == 0)
+    {
+      printf("Buzz\n");
     }
     else
     {
-      if (i % 5 != 0)
-      {
-        printf("Fizz\n");
-      }
-      else
-      {
-        printf("FizzBuzz\n");
-      }
+      printf("%d\n", i);
     }
   }
 }
