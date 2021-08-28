f0b632f92acbf3106cd68b92fac0a4d92ee2ec4f
ailioolio <aiai4849@gmail.com>
2021-08-28 19:33:21 +0900

    solve 0827

---
+++ ailioolio/0827.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("整数を入力してください: ");
+
+  int num;
+  scanf("%d", &num);
+
+  int count = 0;
+  for (int i = 1; i <= num; i++) {
+    int sum = 0;
+
+    for (int j = 1; j <= i; j++) {
+      if (i % j == 0){
+        sum++;
+      }
+
+    }
+
+    if (sum == 2) {
+      count++;
+    }
+  }
+
+  printf("素数の数は%dです\n", count);
+}
