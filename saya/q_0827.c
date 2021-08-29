19dec4317914a3d6633f48075157997ba8727d81
26sayaka <kyukodai260512@gmail.com>
2021-08-30 08:21:46 +0900

    Solve q_0826 and Solve q_0827

---
+++ saya/q_0827.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int is_prime(int *num) {
+    if (*num == 2) {
+        return 1;
+    }
+    else if (*num % 2 == 0 || *num < 2) {
+        return 0;
+    }
+    else {
+        int i;
+        for (i = 3; i < *num; i += 2) {
+            if (*num % i == 0) {
+                return 0;
+            }            
+        }
+    }
+    return 1;
+}
+
+int main(void){
+	printf("整数を入力してください．\n");
+	int num;
+	scanf("%i", &num);
+
+    int count = 0;
+    int i;
+    for (i = 2; i <= num; i++) {
+        if (is_prime(&i) == 1) {
+            count += 1;
+        }
+        else {
+            continue;
+        }
+    }
+
+    printf("%i以下の素数は%i個あります.\n", num, count);
+
+    return 0;
+}
