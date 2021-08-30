bce8ae78b71f74859afd2a38de18e477fcd42452
ailioolio <aiai4849@gmail.com>
2021-08-30 12:08:26 +0900

    use break

--- ailioolio/0827.c
+++ ailioolio/0827.c
@@ -3,23 +3,39 @@
 int main(void) {
   printf("整数を入力してください: ");
 
-  int num;
+  int num, count;
   scanf("%d", &num);
 
-  int count = 0;
-  for (int i = 1; i <= num; i++) {
-    int sum = 1;
+  switch (num) {
+    case 1:
+      count = 0;
+      break;
 
-    for (int j = 1; j < i; j++) {
-      if (i % j == 0) {
-        sum++;
+    case 2:
+      count = 1;
+      break;
+
+    default:
+      count = 1;
+      for (int i = 3; i <= num; i++) {
+        int sum = 2;
+
+        for (int j = 2; j < i; j++) {
+          if (i % j == 0) {
+            sum++;
+            break;
+          }
+        }
+
+        if (sum == 2) {
+          count++;
+        }
       }
-    }
-
-    if (sum == 2) {
-      count++;
-    }
+      break;
   }
 
+
   printf("素数の数は%dです\n", count);
+
+  return 0;
 }
