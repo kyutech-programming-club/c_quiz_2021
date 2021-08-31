5bfad71e1d3a3125ffcb31e25543de171e829d2d
26sayaka <kyukodai260512@gmail.com>
2021-08-31 23:30:34 +0900

    change indent

--- saya/q_0828.c
+++ saya/q_0828.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 
 int main(void){
-    printf("整数を入力してください．\n");
-	int num;
-	scanf("%i", &num);
+  printf("整数を入力してください．\n");
+  int num;
+  scanf("%i", &num);
 
-    int div_count = 0;  //div = divisor
-    int max_div_count = 0;
-    int final;
-    int i, j;
-	for (i = 1; i <= num; i++) {
-		for (j = 1; j <= i; j++) {
-            if (i % j == 0) {
-                div_count += 1;
-            }
-        }
-        if (max_div_count <= div_count) {
-            max_div_count = div_count;
-            final = i;
-        }
-        div_count = 0;
-	}
+  int div_count = 0;  //div = divisor
+  int max_div_count = 0;
+  int final;
+  int i, j;
+  for (i = 1; i <= num; i++) {
+    for (j = 1; j <= i; j++) {
+      if (i % j == 0) {
+        div_count += 1;
+      }
+    }
 
-    printf("%i以下の整数の内、最も約数の多い数は%i.\n", num, final);
+    if (max_div_count <= div_count) {
+      max_div_count = div_count;
+      final = i;
+    }
+    
+    div_count = 0;
+  }
 
-    return 0;
+  printf("%i以下の整数の内、最も約数の多い数は%i.\n", num, final);
+
+  return 0;
 }
