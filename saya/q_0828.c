a648989170bff1fed87b435f918cc63fe49f10de
26sayaka <kyukodai260512@gmail.com>
2021-08-31 20:18:12 +0900

    Change the variable name

--- saya/q_0828.c
+++ saya/q_0828.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 int main(void){
-	printf("整数を入力してください．\n");
+    printf("整数を入力してください．\n");
 	int num;
 	scanf("%i", &num);
 
-    int count1 = 0;
-    int count2 = 0;
+    int div_count = 0;  //div = divisor
+    int max_div_count = 0;
     int final;
     int i, j;
 	for (i = 1; i <= num; i++) {
 		for (j = 1; j <= i; j++) {
             if (i % j == 0) {
-                count1 += 1;
+                div_count += 1;
             }
         }
-        if (count2 <= count1) {
-            count2 = count1;
+        if (max_div_count <= div_count) {
+            max_div_count = div_count;
             final = i;
         }
-        count1 = 0;
+        div_count = 0;
 	}
 
     printf("%i以下の整数の内、最も約数の多い数は%i.\n", num, final);
