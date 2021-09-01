57254eb162aca008d27a4065af41213b8d4bca1f
26sayaka <kyukodai260512@gmail.com>
2021-09-01 20:55:20 +0900

    Solve q_0829

---
+++ saya/q_0829.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void){
+    printf("整数を入力してください。\n");
+
+    int num[3];
+    int reverse[3];
+    int i;
+    for (i = 0; i < 3; i++) {
+        scanf("%i", &num[i]);
+        printf("num[%i] = %i\n", i, num[i]);  //入力確認用
+    }
+
+    printf("逆順にすると");
+    for (i = 0; i < 3; i++) {
+        reverse[i] = num[3 - 1 - i];
+        printf("%i", reverse[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
