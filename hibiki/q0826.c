d75eb2e94e6cde2bafcd0ca0fcae1317814066d6
hibiki1228 <devkimadore1228@gmail.com>
2021-10-02 23:33:56 +0900

    answer q0824-0826

---
+++ hibiki/q0826.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  int num, sum=0;
+
+  printf("Enter a number : ");
+  scanf("%d", &num);
+
+  for (int i = 1 ; i <= num; i++) {
+    if (num % i == 0) {
+      sum += i;
+    } else {
+      continue;
+    }
+  }
+
+  printf("sum of divisors is %d\n", sum);
+  return 0;
+}
